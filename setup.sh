a401de4ad3dea009a9cef4447e7d09bab562244c
Ray <94271266+Rayfor000@users.noreply.github.com>
2024-12-25 17:18:34 +0800

    Beta173

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 
 Authors="OGATA Open-Source"
 Scripts="panelbase-install.sh"
-Version="Beta172"
+Version="Beta173"
 License="Apache License 2.0"
 
 CLR1="\033[0;31m"
@@ -227,12 +227,7 @@ alias.url = ( "/cgi-bin/" => "$INSTALL_DIR/cgi-bin/" )
 	cgi.assign = ( "" => "" )
 	setenv.add-response-header = (
 		"Cache-Control" => "no-cache",
-		"X-Accel-Buffering" => "no",
-		"X-Frame-Options" => "SAMEORIGIN",
-		"X-Content-Type-Options" => "nosniff",
-		"X-XSS-Protection" => "1; mode=block",
-		"Referrer-Policy" => "strict-origin-when-cross-origin",
-		"Content-Security-Policy" => "default-src 'self'"
+		"X-Accel-Buffering" => "no"
 	)
 }
 
@@ -252,15 +247,17 @@ mimetype.assign = (
 	".eot"  => "application/vnd.ms-fontobject"
 )
 
-include_shell "echo '\$HTTP[\"url\"] =~ \"\.(' $(grep '^STATIC_RESOURCES=' $INSTALL_DIR/config/security.conf | cut -d'\"' -f2) ')\$\" {}'"
+\$HTTP["url"] =~ "\.(png|ico|jpg|jpeg|gif|css|js|woff|woff2|ttf|eot|svg)\$" {}
 
-include_shell "echo '\$HTTP[\"url\"] !~ \"' $(grep '^PUBLIC_ROUTES=' $INSTALL_DIR/config/security.conf | cut -d'\"' -f2) '\" {'"
-include_shell "echo '  \$HTTP[\"url\"] !~ \"\.(' $(grep '^STATIC_RESOURCES=' $INSTALL_DIR/config/security.conf | cut -d'\"' -f2) ')\$\" {'"
-echo '    url.rewrite-once = ('
-echo '      "^/.*" => "/cgi-bin/check_auth.cgi"'
-echo '    )'
-echo '  }'
-echo '}'
+\$HTTP["url"] !~ "^/\$" {
+	\$HTTP["url"] !~ "^/cgi-bin/(auth|panel)\.cgi" {
+		\$HTTP["url"] !~ "\.(png|ico|jpg|jpeg|gif|css|js|woff|woff2|ttf|eot|svg)\$" {
+			url.rewrite-once = (
+				"^/.*" => "/cgi-bin/check_auth.cgi"
+			)
+		}
+	}
+}
 
 index-file.names = ( "index.html" )
 
