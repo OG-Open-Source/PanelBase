874fa9e1f3cf9c10f912e748f11f6e21662ec08a
Ray <94271266+Rayfor000@users.noreply.github.com>
2025-02-21 20:57:32 +0800

    2025-02-21

---
+++ commands/time.go
@@ -0,0 +1,18 @@
+# @commands: time
+# @pkg_manager: apk, apt, opkg, pacman, yum, zypper, dnf
+# @dependencies: golang
+# @author: PanelBase Team
+# @version: 1.0.0
+# @description: Show current time
+
+package time
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	fmt.Println("當前時間：", now.Format("2006-01-02 15:04:05"))
+}
